39835cb1db76733041f91bcd540c8a0cb7a23848
Shane Rosenthal <srosenthal82@gmail.com>
2024-10-09 15:45:37 -0400

    updates

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,8 +17,8 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Tixel',
-            'email' => 'admin@tixel.com',
+            'name' => 'Admin',
+            'email' => 'admin@papashanes.com',
             'password' => Hash::make('password'),
         ]);
 
